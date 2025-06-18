4885f226d99b2bcdf63fdd03b81b22e102896cfc
Mafe Duarte A <mfda.1396@gmail.com>
2025-06-18 14:53:22 +0000

    semaforo con uso de hooks

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+  const [color, setColor] = useState("red");
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const lightStyle = (lightColor) => ({
+    width: "100px",
+    height: "100px",
+    borderRadius: "50%",
+    margin: "10px",
+    backgroundColor: lightColor,
+    opacity: color === lightColor ? 1 : 0.3, //aqui esta como la logica de que si esa condicion se cumple, entonces la opacidad sera 1 haciendo uso de el operador ternario y se vera brillante y si no entonces 0.3 que es igual a una opacidad baja y se veran apagados los demas.
+    cursor: "pointer", // para que se vea como una manita cuando se pase sobre los colores.
+    boxShadow: color === lightColor ? `0 0 20px 5px ${lightColor}` : "none", // esto es similar pero ahora con sombra-brillo, si color green es igual a green entonces se aplica la sombra con brillo por ejemplo.
+    transition: "opacity 0.3s, box-shadow 0.3s",
+  });
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "120px", backgroundColor: "black", padding: "20px", borderRadius: "20px" }}>
+      <div
+        style={lightStyle("red")}
+        onClick={() => setColor("red")}
+      ></div>
+      <div
+        style={lightStyle("yellow")}
+        onClick={() => setColor("yellow")}
+      ></div>
+      <div
+        style={lightStyle("green")}
+        onClick={() => setColor("green")}
+      ></div>
+    </div>
+  );
+}
 
 export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
@@ -14,5 +15,5 @@ import Home from './components/Home';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home/>
-  </React.StrictMode>,
-)
+  </React.StrictMode>, 
+) //acerca de esto, lo investigue mas a fondo porque me gusta entender el porque de todo y basicamente Busca el elemento con id root en mi HTML, y dentro de él, renderiza (muestra en pantalla) el componente home (mi semáforo)
